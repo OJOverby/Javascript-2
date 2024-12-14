304088f3d9f94b43b8a97bbd2a5c241aefecd63f
Ole Jørgen <quezt@outlook.com>
2024-12-14 20:50:37 +0100

    Autologin
    Added autologin if you have valid token

---
+++ js/api-calls/loggedInCheck.js
@@ -0,0 +1,32 @@
+import { load } from "../functions/load.js";
+import { API_KEY } from "../api/api-key.js";
+
+export async function loggedInCheck() {
+  const token = load("token");
+
+  if (!token) {
+    return false;
+  }
+
+  try {
+    const response = await fetch(BASE_API + "/social/posts/", {
+       headers: {
+        Authorization: `Bearer ${load("token")}`,
+        "X-Noroff-API-Key": API_KEY,
+        "Content-Type": "application/json",
+              },
+      method: "GET",
+    });
+
+    if (response.ok) {
+        window.location.href = "/feed/index.html";
+      return true; 
+    }
+  } catch (error) {
+    console.error("Error validating token:", error);
+  }
+  return false;
+}
+loggedInCheck();
+
+
